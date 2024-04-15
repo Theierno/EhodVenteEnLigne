989b130c310cebcc94815ae9d2013a1779f533ef
Thierno diallo <07j01ismaila@gmail.com>
2024-04-15 00:17:17 +0000

    finalisation de l'internationalisation des messages d'erreurs un bug rencontrer lors du commit passé mais maintenant c'est regler les erreurs s'affichent en fonction de la langue courante

--- EhodVenteEnLigne/Models/ViewModels/ProductViewModel.cs
+++ EhodVenteEnLigne/Models/ViewModels/ProductViewModel.cs
@@ -1,20 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.ComponentModel.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.Localization;
 
 namespace EhodBoutiqueEnLigne.Models.ViewModels
 {
     public class ProductViewModel
     {
+
         [BindNever]
         public int Id { get; set; }
 
+        [Required(ErrorMessageResourceType = typeof(EhodVenteEnLigne.Resources.Models.Product), ErrorMessageResourceName = "ErrorMissingName")] 
         public string Name { get; set; }
 
         public string Description { get; set; }
 
         public string Details { get; set; }
 
+        [Required(ErrorMessageResourceType = typeof(EhodVenteEnLigne.Resources.Models.Product), ErrorMessageResourceName = "ErrorMissingStock")]
+
+        [Range(1, int.MaxValue, ErrorMessageResourceType = typeof(EhodVenteEnLigne.Resources.Models.Product), ErrorMessageResourceName = "ErrorStockValue")]
+
         public string Stock { get; set; }
 
+        [Required(ErrorMessageResourceType = typeof(EhodVenteEnLigne.Resources.Models.Product), ErrorMessageResourceName = "ErrorMissingPrice")]
+        [Range(0.01, double.MaxValue, ErrorMessageResourceType = typeof(EhodVenteEnLigne.Resources.Models.Product), ErrorMessageResourceName = "ErrorPriceValue")]
         public string Price { get; set; }
     }
 }
