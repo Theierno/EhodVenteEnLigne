45f71a5f60c70d0cce3a1ccb315f8bd38b5d01c5
Thierno diallo <07j01ismaila@gmail.com>
2024-04-16 01:23:26 +0000

    Module test terminé + fin du projet

--- EhodVenteEnLigne.Tests/ProductServiceTests.cs
+++ EhodVenteEnLigne.Tests/ProductServiceTests.cs
@@ -1,11 +1,10 @@
-﻿using EhodBoutiqueEnLigne.Models.Repositories;
+﻿using EhodBoutiqueEnLigne.Models;
+using EhodBoutiqueEnLigne.Models.Repositories;
 using EhodBoutiqueEnLigne.Models.Services;
-using EhodBoutiqueEnLigne.Models;
 using EhodBoutiqueEnLigne.Models.ViewModels;
 using Microsoft.Extensions.Localization;
 using Moq;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using Xunit;
 
 namespace EhodBoutiqueEnLigne.Tests
@@ -25,17 +24,23 @@ namespace EhodBoutiqueEnLigne.Tests
             _mockOrderRepository = new Mock<IOrderRepository>();
             _mockLocalizer = new Mock<IStringLocalizer<ProductService>>();
             _mockLocalizer.Setup(l => l["MissingName"]).Returns(new LocalizedString("MissingName", "MissingName"));
+            _mockLocalizer.Setup(l => l["MissingQuantity"]).Returns(new LocalizedString("MissingQuantity", "MissingQuantity"));
+            _mockLocalizer.Setup(l => l["MissingPrice"]).Returns(new LocalizedString("MissingPrice", "MissingPrice"));
+            _mockLocalizer.Setup(l => l["PriceNotANumber"]).Returns(new LocalizedString("PriceNotANumber", "PriceNotANumber"));
+            _mockLocalizer.Setup(l => l["PriceNotGreaterThanZero"]).Returns(new LocalizedString("PriceNotGreaterThanZero", "PriceNotGreaterThanZero"));
+            _mockLocalizer.Setup(l => l["StockNotGreaterThanZero"]).Returns(new LocalizedString("StockNotGreaterThanZero", "StockNotGreaterThanZero"));
+            _mockLocalizer.Setup(l => l["StockNotAnInteger"]).Returns(new LocalizedString("StockNotAnInteger", "StockNotAnInteger"));
 
             _productService = new ProductService(_mockCart.Object, _mockProductRepository.Object, _mockOrderRepository.Object, _mockLocalizer.Object);
         }
 
         [Fact]
-        public void SaveProduct_MissingName_ShouldReturnErrorMessage()
+        public void SaveProduct_AllFieldsFilled_ShouldNotReturnErrorMessages()
         {
             // Arrange
             var productViewModel = new ProductViewModel
             {
-                Name = "test",
+                Name = "Test Product",
                 Description = "Test Description",
                 Price = "10,99",
                 Stock = "5"
@@ -46,11 +51,149 @@ namespace EhodBoutiqueEnLigne.Tests
             var modelErrors = _productService.CheckProductModelErrors(productViewModel);
 
             // Assert
-            
-            Assert.Contains("MissingName", modelErrors["MissingName"]); // Vérifie que la liste d'erreurs associée à "MissingName" contient l'erreur parceque au niveau de mon service la methode CheckProductModelErrors renvoie un dictionnaire
-
-            // TODO écrire les méthodes de test pour assurer une couverture correcte de toutes les possibilités
+            Assert.Empty(modelErrors);
         }
+
+        [Fact]
+        public void SaveProduct_MissingName_ShouldReturnErrorMessage()
+        {
+            // Arrange
+            var productViewModel = new ProductViewModel
+            {
+                Name = " ",
+                Description = "Test Description",
+                Price = "10,99",
+                Stock = "5"
+            };
+
+            // Act
+            _productService.SaveProduct(productViewModel);
+            var modelErrors = _productService.CheckProductModelErrors(productViewModel);
+
+            // Assert
+            Assert.Contains("MissingName", modelErrors["MissingName"]);
+        }
+
+        [Fact]
+        public void SaveProduct_MissingStock_ShouldReturnErrorMessage()
+        {
+            // Arrange
+            var productViewModel = new ProductViewModel
+            {
+                Name = "Test Name ",
+                Description = "Test Description",
+                Price = "10,99",
+                Stock = ""
+            };
+
+            // Act
+            _productService.SaveProduct(productViewModel);
+            var modelErrors = _productService.CheckProductModelErrors(productViewModel);
+
+            // Assert
+            Assert.Contains("MissingQuantity", modelErrors["MissingQuantity"]);
+        }
+
+        [Fact]
+        public void SaveProduct_StockNotAnInteger_ShouldReturnErrorMessage()
+        {
+            // Arrange
+            var productViewModel = new ProductViewModel
+            {
+                Name = "Test Name ",
+                Description = "Test Description",
+                Price = "10,99",
+                Stock = "1,3"
+            };
+
+            // Act
+            _productService.SaveProduct(productViewModel);
+            var modelErrors = _productService.CheckProductModelErrors(productViewModel);
+
+            // Assert
+            Assert.Contains("StockNotAnInteger", modelErrors["StockNotAnInteger"]);
+        }
+
+        [Fact]
+        public void SaveProduct_StockNotGreaterThanZero_ShouldReturnErrorMessage()
+        {
+            // Arrange
+            var productViewModel = new ProductViewModel
+            {
+                Name = "Test Name ",
+                Description = "Test Description",
+                Price = "10,99",
+                Stock = "-1"
+            };
+
+            // Act
+            _productService.SaveProduct(productViewModel);
+            var modelErrors = _productService.CheckProductModelErrors(productViewModel);
+
+            // Assert
+            Assert.Contains("StockNotGreaterThanZero", modelErrors["StockNotGreaterThanZero"]);
+        }
+
+        [Fact]
+        public void SaveProduct_MissingPrice_ShouldReturnErrorMessage()
+        {
+            // Arrange
+            var productViewModel = new ProductViewModel
+            {
+                Name = "Test Name ",
+                Description = "Test Description",
+                Price = " ",
+                Stock = "1"
+            };
+
+            // Act
+            _productService.SaveProduct(productViewModel);
+            var modelErrors = _productService.CheckProductModelErrors(productViewModel);
+
+            // Assert
+            Assert.Contains("MissingPrice", modelErrors["MissingPrice"]);
+        }
+
+        [Fact]
+        public void SaveProduct_PriceNotANumber_ShouldReturnErrorMessage()
+        {
+            // Arrange
+            var productViewModel = new ProductViewModel
+            {
+                Name = "Test Name ",
+                Description = "Test Description",
+                Price = "a",
+                Stock = "1"
+            };
+
+            // Act
+            _productService.SaveProduct(productViewModel);
+            var modelErrors = _productService.CheckProductModelErrors(productViewModel);
+
+            // Assert
+            Assert.Contains("PriceNotANumber", modelErrors["PriceNotANumber"]);
+        }
+
+        [Fact]
+        public void SaveProduct_PriceNotGreaterThanZero_ShouldReturnErrorMessage()
+        {
+            // Arrange
+            var productViewModel = new ProductViewModel
+            {
+                Name = "Test Name ",
+                Description = "Test Description",
+                Price = "0",
+                Stock = "1"
+            };
+
+            // Act
+            _productService.SaveProduct(productViewModel);
+            var modelErrors = _productService.CheckProductModelErrors(productViewModel);
+
+            // Assert
+            Assert.Contains("PriceNotGreaterThanZero", modelErrors["PriceNotGreaterThanZero"]);
+        }
+
 
     }
 }
